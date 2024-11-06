c89398e6f22c123efd98df7c9541e9a8e10f4067
Ivan Borisenko <shimy92@mail.ru>
2024-11-06 14:54:46 +0300

    Didn't complete quiz, faq and products

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const buttonBurger = document.querySelector(".burger");
+const menuBurger = document.querySelector(".burger-menu");
+
+buttonBurger.addEventListener("click", () => {
+  buttonBurger.classList.toggle("active");
+  menuBurger.classList.toggle("active");
+});
