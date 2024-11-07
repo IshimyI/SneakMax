f09f75f74a4ae0e4e1724540954d570e2349fd1e
Ivan Borisenko <shimy92@mail.ru>
2024-11-07 15:36:41 +0300

    products completed

--- scripts.js
+++ scripts.js
@@ -5,3 +5,13 @@ buttonBurger.addEventListener("click", () => {
   buttonBurger.classList.toggle("active");
   menuBurger.classList.toggle("active");
 });
+
+const buttonOpenProducts = document.querySelector(".products-button");
+const products = document.querySelectorAll(".products-item-2");
+
+buttonOpenProducts.addEventListener("click", () => {
+  products.forEach((product) => {
+    product.classList.remove("products-item-2");
+  });
+  buttonOpenProducts.classList.add("products-button-open");
+});
