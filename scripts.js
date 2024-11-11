90723529f2b1162cc1dc1171a091383f07e15f72
Ivan Borisenko <shimy92@mail.ru>
2024-11-11 22:55:04 +0300

    hovering products comleting

--- scripts.js
+++ scripts.js
@@ -4,7 +4,18 @@ const menuBurger = document.querySelector(".burger-menu");
 buttonBurger.addEventListener("click", () => {
   buttonBurger.classList.toggle("active");
   menuBurger.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
 });
+
+// const productsChoose = document.querySelectorAll(
+//   ".products-item-img",
+//   ".products-item-btns"
+// );
+// productsImage("mouseover", () => {
+//   productsImage.forEach((el) => {
+//     el.classList.toggle("opacity");
+//   });
+// });
 
 const buttonOpenProducts = document.querySelector(".products-button");
 const products = document.querySelectorAll(".products-item-2");
@@ -14,4 +25,28 @@ buttonOpenProducts.addEventListener("click", () => {
     product.classList.remove("products-item-2");
   });
   buttonOpenProducts.classList.add("products-button-open");
+});
+
+const buttonFaq1 = document.querySelector(".faq-button-1");
+const buttonFaq2 = document.querySelector(".faq-button-2");
+
+const answer1 = document.querySelector(".faq-answer-1");
+const answer2 = document.querySelector(".faq-answer-2");
+
+buttonFaq1.addEventListener("click", () => {
+  answer1.classList.toggle("faq-answer-open");
+  buttonFaq1.classList.toggle("faq-button-open");
+});
+
+buttonFaq2.addEventListener("click", () => {
+  answer2.classList.toggle("faq-answer-open");
+  buttonFaq2.classList.toggle("faq-button-open");
+});
+
+const ctaForm = document.querySelector("#cta-form");
+ctaForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const formData = new FormData(ctaForm);
+  const data = Object.fromEntries(formData.entries());
+  alert(JSON.stringify(data));
 });
